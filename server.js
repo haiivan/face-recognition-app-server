1bc3f850d5d678959a334d4b2d25511c3310c11b
HaIvanVillegas <alberto_ha@live.com>
2020-02-02 19:44:25 -0600

    PORT changes

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ app.post("/imageUrl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(4000, () => {
-  console.log("App is running on port 4000 ");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App is running on port ${process.env.PORT} `);
 });
 
 /* 
